5dcecb64b2d1c3c48b40b28b8c8bdbceb7c40ac3
Michael Bialowas <36966595+lmhs-mbialowas@users.noreply.github.com>
2018-03-01 13:17:23 -0500

    Divide for people

---
+++ html/all/Michael/functions.js
@@ -0,0 +1,83 @@
+//EDIT THIS VARIABLE:
+var numberOfPagesInThisFolder = 43;
+
+var randomPage;
+var randomPageNumber;
+
+var pagesAvailable = new Array(numberOfPagesInThisFolder);
+           
+    pagesAvailable[0]="ads.html"; //EDIT ALL OF THESE TOO
+    pagesAvailable[1]="adsForFreeStuff.html";
+    pagesAvailable[2]="apps.html";
+    pagesAvailable[3]="avoidCyberbullying.html";
+    pagesAvailable[4]="inappropriateWebsites.html";
+    pagesAvailable[5]="avoidCyberbullying.html";
+    pagesAvailable[6]="avoidPlagarism.html";
+    pagesAvailable[7]="backdoors.html";
+    pagesAvailable[8]="backUpFiles.html";
+    pagesAvailable[9]="beSafeOnline.html";
+    pagesAvailable[10]="botNet.html";
+    pagesAvailable[11]="computerWorms.html";
+    pagesAvailable[12]="credit.html";
+    pagesAvailable[13]="deleted.html";
+    pagesAvailable[14]="DOS.html";
+    pagesAvailable[15]="fakeAds.html";
+    pagesAvailable[16]="fakeWebsites.html";
+    pagesAvailable[17]="gamblingOnline.html";
+    pagesAvailable[18]="inappropriateWebsites.html";
+    pagesAvailable[19]="keyLoggers.html";
+    pagesAvailable[20]="mobileThreats.html";
+    pagesAvailable[21]="passwords.html";
+    pagesAvailable[22]="personalInformation.html";
+    pagesAvailable[23]="pharming.html"; 
+    pagesAvailable[24]="phishing.html";
+    pagesAvailable[25]="pictures.html";
+    pagesAvailable[26]="pirating.html";
+    pagesAvailable[27]="plagarism.html";
+    pagesAvailable[28]="racism.html";
+    pagesAvailable[29]="ransomeWare.html"; 
+    pagesAvailable[30]="regularlyScanYourDevicesFiles.html";
+    pagesAvailable[31]="resetAllPasswords.html";
+    pagesAvailable[32]="rogueSecuritySoftware.html";
+    pagesAvailable[33]="rootkits.html";
+    pagesAvailable[34]="safeSites.html";
+    pagesAvailable[35]="settings.html";
+    pagesAvailable[36]="spamEmail.html";
+    pagesAvailable[37]="spoofing.html";
+    pagesAvailable[38]="spyware.html";
+    pagesAvailable[39]="threatsToMobilePayment.html";
+    pagesAvailable[40]="updateYourOperatingSystem.html";
+    pagesAvailable[41]="WiFiEavesdropping.html";
+    pagesAvailable[42]="WPA2Handshake.html";
+
+
+function showDiv() {
+    var divToShow = document.getElementById("moreInfo");
+    if (divToShow.style.display == "none") {
+        divToShow.style.display = "block";
+    } else {
+        divToShow.style.display = "none";
+    }
+}
+
+function generateRandomNumber(){
+    var randomNumber = -1;
+    while (randomNumber < 0 || randomNumber > numberOfPagesInThisFolder || isNaN(randomNumber)){
+        randomNumber = parseInt(Math.random()*(numberOfPagesInThisFolder+1));
+    }
+    return randomNumber;
+}
+
+function generateRandomPage(){
+    randomPageNumber = generateRandomNumber();
+    randomPage = pagesAvailable[randomPageNumber];
+    return randomPage;
+}
+
+function showNewCard(){
+    
+    numberOfPagesInThisFolder--;
+        
+    document.location.href = generateRandomPage();
+}
+
