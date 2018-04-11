9db14aa3a2eab4ff9d89d92653abd7d9bc88734d
Konnor Klercke (PrototypeXenon) <konnorklercke@gmail.com>
2018-04-11 13:54:38 -0400

    :file_folder: Consolidate JS files

--- html/functions.js
+++ html/functions.js
@@ -1,5 +1,4 @@
-//EDIT THIS VARIABLE:
-var numberOfPagesInThisFolder = 43;
+var numberOfPagesInThisFolder = 41;
 
 var randomPage;
 var randomPageNumber;
@@ -79,3 +78,37 @@ function showNewCard(){
     document.location.href = generateRandomPage();
 }
 
+function searchFor() {
+    var input, filter, ul, li, a, i;
+    input = window.document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("searchOptions");
+    li = ul.getElementsByTagName('li');
+    
+    $(ul).removeClass('hidden');
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        //li[i].style.display = "none";
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function onKonamiCode(cb) {
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+onKonamiCode(function () {alert('Sneaky!')})
